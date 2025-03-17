ff6f32505a6fbd21bb6580f37171ab920b7bdaec
Michael A <azilinon.michael@gmail.com>
2025-03-17 19:08:43 +0100

    create mockupData file

---
+++ src/utils/mockupData.js
@@ -0,0 +1,37 @@
+export const servicesData = [
+    {
+        id: 1,
+        title: "Rekonstrukce Rodinného domu",
+        caption: "Kompletní rekonstrukce rodinných domů včetně fasád, střech a interiérů. Zajistíme modernizaci s důrazem na energetickou úsporu."
+    },
+    {
+        id: 2,
+        title: "Rekonstrukce Bytové jednotky",
+        caption: "Obnovíme váš byt od základů - nové podlahy, elektroinstalace, kuchyně i koupelny. Přeměníme staré prostory v nový moderní domov."
+    },
+    {
+        id: 3,
+        title: "Rekonstrukce Společných prostor",
+        caption: "Modernizace společných prostor v bytových domech, včetně chodeb, výtahových šachet a sklepních prostor. Zlepšíme estetiku i funkčnost."
+    },
+    { 
+        id: 4,
+        title: "Novostavba na klíč",
+        caption: "Postavíme vám nový dům podle vašich představ. Od projektu po finální realizaci – kvalitní a bezstarostné řešení na míru." 
+    },
+    { 
+        id: 5,
+        title: "Půdní přestavby",
+        caption: "Vytvoříme z nevyužitého podkroví krásné obytné prostory. Maximální využití prostoru s důrazem na světlo a komfort."
+    },
+    { 
+        id: 6,
+        title: "Elektroinstalace a Vodoinstalace",
+        caption: "Nové rozvody elektřiny, vody a plynu v rámci rekonstrukcí i novostaveb. Používáme moderní a bezpečné technologie."
+    },
+    {
+        id: 7,
+        title: "Rekonstrukce kuchyně, koupelny",
+        caption: "Kompletní renovace kuchyní a koupelen včetně obkladů, sanitární techniky a elektroinstalací. Elegantní design a praktické řešení."
+    }
+];
