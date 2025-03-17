8419d28f19a07e18624f909ed8979491470af139
František Majer <frantisek@brightenevo.com>
2025-03-17 22:30:30 +0100

    remove unnecessary css from service component

--- src/components/Body/Services.jsx
+++ src/components/Body/Services.jsx
@@ -17,13 +17,13 @@ const Services = () => {
 
     return (
         <Container ref={componentRef} className='services-container' fluid>
-            <div className='justify-content-center base-width p-0 service-grid'>
+            <div className='justify-content-center base-width service-grid'>
                 {servicesData.map((service, index) => {
                     if (index > 2 && !showServices)
                         return;
                     else 
                         return (
-                            <div key={service.id} md={6} lg={4} className="">
+                            <div key={service.id}>
                                 <Card className="service-card">
                                     <img src={service.img} className='service-image'/>
                                 </Card>
